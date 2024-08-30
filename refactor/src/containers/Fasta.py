b550761620cb18f4d570f53dfdcda514dab3a7c7
Nobody <mishasikolenko@mail.ru>
2024-08-30 13:00:36 +0300

    Create Fasta container

---
+++ refactor/src/containers/Fasta.py
@@ -0,0 +1,8 @@
+class Fasta():
+    def __init__(self, label : str, seq : str, fatataHeader : str):
+        self.label = label
+        self.seq = seq
+        self.fatataHeader = fatataHeader
+    #end def
+#end class
+
